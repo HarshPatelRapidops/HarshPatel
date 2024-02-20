ecb20876ca8e81ecb5533f297562435a9a9ae71a
Harsh Patel <harsh.d.patel@rapidops.com>
2024-02-20 15:22:21 +0530

    React Exercise 2

---
+++ React/exercise_2/src/App.js
@@ -0,0 +1,12 @@
+import logo from './logo.svg';
+import DynamicTable from './DynamicTable';
+
+function App() {
+  return (
+    <div className="App">
+      <DynamicTable />
+    </div>
+  );
+}
+
+export default App;
